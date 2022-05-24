67b55fbe2ece487d9d2626e130da82c38db54b72
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-05-24 22:11:00 +0200

    styling 2nd

--- pages/einar.js
+++ pages/einar.js
@@ -9,9 +9,9 @@ export default function einar({ posts }) {
         <div className='mt-5'>
             {posts.map((post, index) => (
                 <Link href={'/blog/' + post.slug} passHref key={index}>
-                    <div className="relative flex flex-col min-w-0 mb-3 cursor-pointer break-words" style={{ maxWidth: '540px' }}>
-                        <div className="row g-0">
-                            <div className="col-md-8">
+                    <div className=" bg-[#f6f6f6] bg-clip-border border-gray-900 p-4 rounded-2xl relative border-solid flex flex-col min-w-0 mb-3 cursor-pointer break-words  max-w-[540px]" >
+                        <div className="row gap-y-4">
+                            <div className="flex w-4/6">
                                 <div className="card-body">
                                     <h5 className="card-title">{post.frontMatter.title}</h5>
                                     <p className="card-text">{post.frontMatter.description}</p>
