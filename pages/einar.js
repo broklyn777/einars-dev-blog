b0d88c4432d7b9653d232080f584c89c333dcbfb
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-05-25 01:08:33 +0200

    added list card

--- pages/einar.js
+++ pages/einar.js
@@ -9,21 +9,21 @@ export default function einar({ posts }) {
         <div className='mt-5'>
             {posts.map((post, index) => (
                 <Link href={'/blog/' + post.slug} passHref key={index}>
-                    <div className=" bg-[#f6f6f6] bg-clip-border border-gray-900 p-4 rounded-2xl relative border-solid flex flex-col min-w-0 mb-3 cursor-pointer break-words  max-w-[540px]" >
-                        <div className="row gap-y-4">
-                            <div className="flex w-4/6">
-                                <div className="card-body">
-                                    <h5 className="card-title">{post.frontMatter.title}</h5>
-                                    <p className="card-text">{post.frontMatter.description}</p>
-                                    <p className="card-text">
-                                        <small className="text-muted">{post.frontMatter.date}</small>
+                    <div className="  bg-clip-border  rounded-md relative px-3 md:px-0 flex flex-col min-w-0 mb-3 cursor-pointer break-words  max-w-[540px]" >
+                        <div className="gap-y-4 flex w-full pl-3  bg-white rounded-lg shadow-md mt-6">
+                            <div className="flex w-4/6 ">
+                                <div className="grow shrink basis-auto p-4">
+                                    <h5 className="text-xl text-[#212529] font-medium mb-2">{post.frontMatter.title}</h5>
+                                    <p className="text-base font-normal text-[#212529]">{post.frontMatter.description}</p>
+                                    <p className="text-[#6c757d] text-base">
+                                        <small className="text-[#6c757d] text-sm">{post.frontMatter.date}</small>
                                     </p>
                                 </div>
                             </div>
-                            <div className="col-md-4 m-auto">
+                            <div className="flex w-1/3 basis-auto grow-0 shrink-0">
                                 <Image
                                     src={post.frontMatter.thumbnailUrl}
-                                    className="img-fluid mt-1 rounded-start"
+                                    className="relative rounded-md p-1 m-0 max-w-full inline-block overflow-hidden box-border"
                                     alt="thumbnail"
                                     width={500}
                                     height={400}
